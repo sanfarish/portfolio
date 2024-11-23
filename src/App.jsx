3cf57c4ea98089b0e952d69becfca92801775717
sanfarish <farisfalah@gmail.com>
2024-11-24 00:51:52 +0700

    add bootstrap, add Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className='bg-body-secondary'>
+      <Nav />
+      <div className='container-fluid'>
+        <h1 className='text-center'>Welcome</h1>
+      </div>
     </div>
   )
 }
