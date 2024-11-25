8a0bfc4b757c22797b37e29ecefa594e145293db
sanfarish <farisfalah@gmail.com>
2024-11-26 00:03:49 +0700

    update skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,11 +3,11 @@ import skills from "../assets/skills.json"
 export default function Skills() {
 
   return (
-    <section className="d-flex flex-column align-items-center py-2 mx-4 border-bottom border-2 border-black">
+    <section className="d-flex flex-column align-items-center pt-2 pb-4 mx-4 border-bottom border-2 border-black">
         <h1>Skills</h1>
         <p>With JavaScript language as my experience, I tend to work with JavaScript based stack such as Node.js as server environment, Express.js for API framework and React.js as frontend user interface. I also learnt HTML5 as web structure and CSS for styling purposes. I used to work with PostgreSQL for RDBMS. I also have experience with Git/Github along with CI/CD pipeline and Nginx configuration.</p>
         <div className="d-flex flex-wrap gap-4 justify-content-center">
-            {skills && skills.map(obj => <div key={skills.indexOf(obj)} className="card text-center" style={{ width: "225px" }}>
+            {skills.map(obj => <div key={skills.indexOf(obj)} className="card text-center" style={{ width: "225px" }}>
                 <div className="card-header">
                     <h4>{obj.title}</h4>
                 </div>
