3bcf6493a0725a39e7bcec560c168146c85bd64e
sanfarish <farisfalah@gmail.com>
2024-11-26 15:16:19 +0700

    update contacts links

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,7 +5,7 @@ export default function Contacts() {
     <section className="text-center pt-2 mx-4" id="contacts" style={{ scrollMarginTop: 65 }}>
       <h1>Contacts</h1>
       <div className="d-flex flex-wrap gap-5 justify-content-center pt-2">
-        <a href="#"
+        <a href="mailto:farisfalah@gmail.com"
           className="link-dark link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
         >
           <figure>
@@ -13,7 +13,7 @@ export default function Contacts() {
             <figcaption className="pt-2"><h4>E-Mail</h4></figcaption>
           </figure>
         </a>
-        <a href="#"
+        <a href="https://www.linkedin.com/in/sanfarish/"
           className="link-dark link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
         >
           <figure>
@@ -21,7 +21,9 @@ export default function Contacts() {
             <figcaption className="pt-2"><h4>Linkedin</h4></figcaption>
           </figure>
         </a>
-        <a href="#" className="link-dark link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-100-hover">
+        <a href="https://github.com/sanfarish"
+          className="link-dark link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
+        >
           <figure>
             <img src="/contacts/github-original.svg" alt="GitHub" height={100} />
             <figcaption className="pt-2"><h4>GitHub</h4></figcaption>
